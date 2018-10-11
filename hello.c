77f853ffd733bd33e39ada85a5e75c2e426f26ab
dudgns7675 <dudgns3153@naver.com>
2018-10-11 11:57:04 +0900

    second edit

--- hello.c
+++ hello.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+void second(){
+	printf("This is Second Branch!!\n");
+}
+
 void myname(){
 	printf("Lee!\n");
 }
 
 int main(){
+	second();
 	printf("Hello Git!\n");
 	myname();
 	return 0;
