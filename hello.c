87fe7e83591f44b431e5ac873348a55f0e9257ed
dudgns7675 <dudgns3153@naver.com>
2018-10-11 11:56:01 +0900

    master edit

--- hello.c
+++ hello.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+void master(){
+	printf("This is Master Branch!!\n");
+}
+
 void myname(){
 	printf("Lee!\n");
 }
 
 int main(){
+	master();
 	printf("Hello Git!\n");
 	myname();
 	return 0;
